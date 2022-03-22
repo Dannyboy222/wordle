bbf1e5a40fe493b541fbd6a72e9709584e59dfd0
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2022-03-21 21:29:42 -0700

    keyboard works when it's clicked

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 	createSquares();
 
+	let guessWords = [[]];
+	let availableSpace = 1;
+
 	const keys = document.querySelectorAll('.keyboard-row button');
 
 	for (let i = 0; i < keys.length; i++) {
 		keys[i].onclick = ({ target }) => {
-			const key = target.getAttribute('data-key');
+			const letter = target.getAttribute('data-key');
 
-			console.log(key);
+			updateGuessWords(letter);
 		};
+	}
+
+	function getCurrentWordArr() {
+		const numberOfGuessWords = guessWords.length;
+		return guessWords[numberOfGuessWords - 1];
+	}
+
+	function updateGuessWords(letter) {
+		const currentWordArr = getCurrentWordArr();
+
+		if (currentWordArr && currentWordArr.length < 5) {
+			currentWordArr.push(letter);
+
+			const availableSpaceEl = document.getElementById(String(availableSpace));
+			availableSpace = availableSpace + 1;
+
+			availableSpaceEl.textContent = letter;
+		}
 	}
 
 	function createSquares() {
