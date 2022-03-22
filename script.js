2602df642d6a0a26315147b73abc7b330e7ec32c
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2022-03-21 21:13:31 -0700

    added function for keyboard when a button is clicked

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 	createSquares();
 
+	const keys = document.querySelectorAll('.keyboard-row button');
+
+	for (let i = 0; i < keys.length; i++) {
+		keys[i].onclick = ({ target }) => {
+			const key = target.getAttribute('data-key');
+
+			console.log(key);
+		};
+	}
+
 	function createSquares() {
 		const gameBoard = document.getElementById('board');
 
